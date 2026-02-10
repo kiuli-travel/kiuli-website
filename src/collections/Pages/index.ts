2fd7595ed193d3095a9df532d1cb20ffcc13a2f8
GrahamWallingtonXeroth <graham@xeroth.ai>
2026-02-10 09:39:43 +0000

    fix: disable autosave to debug page save issue

--- src/collections/Pages/index.ts
+++ src/collections/Pages/index.ts
@@ -144,9 +144,7 @@ export const Pages: CollectionConfig<'pages'> = {
   },
   versions: {
     drafts: {
-      autosave: {
-        interval: 100, // We set this interval for optimal live preview
-      },
+      autosave: false, // Disabled temporarily to debug save issue
       schedulePublish: true,
     },
     maxPerDoc: 50,
